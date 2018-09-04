a05c677143e97a3a330a108088a2b7179d7a4ae7
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-09-04 18:51:57 +0200

    redux setup

--- src/redux/epics/index.js
+++ src/redux/epics/index.js
@@ -1,4 +1,12 @@
 import { combineEpics } from 'redux-observable'
+import { filter, mapTo } from 'rxjs/operators'
 
-export default combineEpics()
+import { PING, pong } from '../actions'
+
+const pingEpic = action$ => action$.pipe(
+  filter(action => action.type === PING),
+  mapTo(pong())
+)
+
+export default combineEpics(pingEpic)
 
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,4 +1,13 @@
 import { combineReducers } from 'redux'
+import { PING } from '../actions'
 
+const pingCount = (state = 0, action) => {
+  switch(action.type) {
+    case PING:
+      return state + 1
+    default:
+      return state
+  }
+}
 
-export default combineReducers()
+export default combineReducers({ pingCount })
