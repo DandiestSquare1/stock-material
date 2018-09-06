27d0cbaeb708ca49612db38b7dbfafb1a3969d2a
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-09-07 00:51:51 +0200

    add placeholder routes and redirect to stock table on unknown route

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles'
 
 import AppBar from './AppBar'
@@ -26,6 +26,10 @@ class App extends Component {
         <div className={classes.mainContent}>
           <Switch>
             <Route exact path='/items' component={StockTable} />
+            <Route exact path='/reports'><div>reports</div></Route>
+            <Route exact path='/labels'><div>labels</div></Route>
+            <Route exact path='/scanner'><div>scanner</div></Route>
+            <Redirect to='/items' />
           </Switch>
         </div>
       </React.Fragment>
