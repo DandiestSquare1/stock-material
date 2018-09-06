245cbe470e9478bb3000ca42df3ded940ada19ff
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-09-07 01:06:38 +0200

    add an add button to the title bar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -8,10 +8,12 @@ import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import List from '@material-ui/core/List'
 import Typography from '@material-ui/core/Typography'
+import Tooltip from '@material-ui/core/Tooltip'
 import MenuIcon from '@material-ui/icons/Menu'
 import LabelIcon from '@material-ui/icons/Label'
 import BarChartIcon from '@material-ui/icons/BarChart'
 import ViewListIcon from '@material-ui/icons/ViewList'
+import AddIcon from '@material-ui/icons/Add'
 import { withStyles } from '@material-ui/core/styles'
 
 import SideLink from './SideLink'
@@ -23,7 +25,11 @@ class AppBar extends React.Component {
 
   static styles = {
     appTitle: {
-      marginLeft: 24
+      marginLeft: 24,
+      flex: '0 0 auto'
+    },
+    spacer: {
+      flex: '1 1 100%'
     }
   }
 
@@ -41,12 +47,22 @@ class AppBar extends React.Component {
       <React.Fragment>
         <Bar position='static'>
           <Toolbar>
-            <IconButton color='inherit' onClick={this.open}>
+            <IconButton
+              color='inherit'
+              onClick={this.open}
+              aria-label='Open navigation bar'
+            >
               <MenuIcon />
             </IconButton>
             <Typography variant='title' color='inherit' className={classes.appTitle}>
               Stock Manager
             </Typography>
+            <div className={classes.spacer} />
+            <Tooltip title='Create a new item'>
+              <IconButton color='inherit' aria-label='Create a new item'>
+                <AddIcon />
+              </IconButton>
+            </Tooltip>
           </Toolbar>
         </Bar>
         <Drawer open={isOpen} onClose={this.close}>
